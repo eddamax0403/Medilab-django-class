6d6fc34586e65eca079babb3131c0b1c57c350c9
eddamax0403 <mwanziaedwin5@gmail.com>
2025-03-03 08:27:03 +0300

    Second commit

--- medapp/models.py
+++ medapp/models.py
@@ -41,3 +41,24 @@ class Ward(models.Model):
 
     def __str__(self):
         return self.name
+
+class Appointment(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    date = models.DateField()
+    department = models.CharField(max_length=50)
+    doctor = models.CharField(max_length=50)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    subject = models.CharField(max_length=50)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
--- medapp/views.py
+++ medapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from  medapp.models import *
 
 # Create your views here.
 
@@ -21,7 +22,35 @@ def doctor(request):
     return render(request,'doctors.html')
 
 def contact(request):
-    return render(request,'contact.html')
+    if request.method == "POST":
+        contactme=Contact(
+          name = request.POST['name'],
+          email = request.POST['email'],
+          subject = request.POST['subject'],
+          message = request.POST['message'],
+        )
+        contactme.save()
+        return redirect('/contact')
+
+    else:
+        return render(request,'contact.html')
+
+
+
 
 def appoint(request):
-    return render(request,'appointment.html')
+    if request.method == 'POST':
+        myappointments = Appointment(
+            name = request.POST['name'],
+            email = request.POST['email'],
+            phone = request.POST['phone'],
+            date = request.POST['date'],
+            department = request.POST['department'],
+            doctor = request.POST['doctor'],
+            message = request.POST['message'],
+        )
+        myappointments.save()
+        return redirect('/appoint')
+
+    else:
+        return render(request,'appointment.html')
